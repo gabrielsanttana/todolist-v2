2f52d896301cbeca97472fa15396bca71c875790
Gabriel Santana <gabriel.gsantana7@gmail.com>
2019-11-26 16:48:00 -0300

    Add gitignore, procfile and update ports

--- server.js
+++ server.js
@@ -138,6 +138,11 @@ app.post("/delete", (req, res) => {
   }
 });
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server is running successfully");
 }); 
